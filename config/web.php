365047b775edf749e34d8a1feb2df2f59306e5d2
23radif <23radif@gmail.com>
2019-05-26 19:53:24 +0300

    .

--- config/web.php
+++ config/web.php
@@ -7,15 +7,25 @@ $config = [
     'language' => 'en',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'bootstrap'],
+    'bootstrap' => [
+        'admin', //mdmsoft/yii2-admin
+        'log',
+        'bootstrap'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
         '@img' => '@app/web/img/',
     ],
+    //mdmsoft/yii2-admin
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ],
+    ],
     'components' => [
 //        Активировать роли RBAC, так же нужно сделать в консоли
         'authManager' => [
+//            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager' //mdmsoft/yii2-admin
             'class' => \yii\rbac\DbManager::class,
         ],
         'i18n' => [
@@ -86,6 +96,20 @@ $config = [
             ],
         ],
 
+    ],
+    //mdmsoft/yii2-admin
+    'as access' => [
+        'class' => 'mdm\admin\classes\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ],
     ],
     'params' => $params,
 ];
